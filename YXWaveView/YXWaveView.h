5092cfd7f0027910d855dd51b70e77e0a88571b1
YourtionGuo <yourtion@gmail.com>
2016-08-26 12:54:31 +0800

    Initial Commit

---
+++ YXWaveView/YXWaveView.h
@@ -0,0 +1,19 @@
+//
+//  YXWaveView.h
+//  YXWaveView
+//
+//  Created by YourtionGuo on 8/26/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YXWaveView.
+FOUNDATION_EXPORT double YXWaveViewVersionNumber;
+
+//! Project version string for YXWaveView.
+FOUNDATION_EXPORT const unsigned char YXWaveViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YXWaveView/PublicHeader.h>
+
+
