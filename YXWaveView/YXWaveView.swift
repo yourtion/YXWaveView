c9c204d1a483867aef9e211ccc00a69b3ae4ea5d
YourtionGuo <yourtion@gmail.com>
2016-08-26 13:00:19 +0800

    update setting

---
+++ YXWaveView/YXWaveView.swift
@@ -0,0 +1,21 @@
+//
+//  YXWaveView.swift
+//  YXWaveView
+//
+//  Created by YourtionGuo on 8/26/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+import UIKit
+
+class YXWaveView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
